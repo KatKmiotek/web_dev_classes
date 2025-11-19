a7b60cfc96868754f80c9618de7eef350a04e353
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2025-11-19 19:02:06 +0000

    docs: console log text

--- module-5-js/examples/iterator/array/script.js
+++ module-5-js/examples/iterator/array/script.js
@@ -1,12 +1,12 @@
 let fruits = ["apple", "banana", "orange"];
 fruits.push("pear")
-console.log("fruits after pear is added", fruits)
+console.log("fruits - pear is added", fruits)
 const len = fruits.length
 console.log("Fruits array has elements: ", len)
 console.log("1st element of fruits array", fruits[0])
 
 function printArrayItems(array) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
   }
 }
