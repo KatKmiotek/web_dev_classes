9295b21a05ed2b0ad1a4790229428943451fe30c
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2026-01-16 20:56:42 +0000

    feat: example

--- module-5-js/examples/fetch/practice/backend/script.js
+++ module-5-js/examples/fetch/practice/backend/script.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const remoteUrl = "https://opentdb.com/api.php?amount=10";
+  const remoteUrl = "https://openlibrary.org/search.json?q=mark+twain";
   const response = await fetch(remoteUrl);
   if (!response.ok)
     throw new Error(`Network response was not ok: ${response.status}`);
@@ -10,19 +10,25 @@ async function getData() {
 
 function renderDataAsText(data) {
   const main = document.querySelector("main") || document.body;
+
   const label = document.createElement('div');
-  label.textContent = 'Data from backend:';
+  label.textContent = 'My new book title is :';
   label.style.marginTop = '1rem';
   label.style.fontWeight = '600';
+
   const container = document.createElement('pre');
   container.style.whiteSpace = 'pre-wrap';
   container.style.background = 'transparent';
-  container.style.border = '1px solid #eee';
+  container.style.border = '1px solid #ca1f1fff';
   container.style.padding = '12px';
   container.style.marginTop = '0.5rem';
   container.style.fontSize = '0.9rem';
-  const toDisplay = data
+
+  const toDisplay = data.docs[3].title
+
+
   container.textContent = JSON.stringify(toDisplay, null, 2);
+
   main.appendChild(label);
   main.appendChild(container);
 }
